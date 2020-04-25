f85cb2deaa1cc0ee6903df6cfc3bda1d1a4218eb
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2020-04-25 21:32:12 +0900

    when typed macro fully changed pray emoji

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,49 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import "./App.css";
 
 function App() {
-  const inputEl = useRef();
-  useEffect(() => {
-    window.addEventListener("keypress", (event) => {
+  const textEl = useRef(null);
+  const [macro, setMacro] = useState("");
+  const handleSpecialKey = useCallback(
+    (event) => {
+      let key = event.keyCode;
+      if (key === 8 && macro.length > 0)
+        setMacro(macro.slice(0, macro.length - 1));
+    },
+    [macro]
+  );
+  const handleUserMacro = useCallback(
+    (event) => {
       let key = event.keyCode;
       let chr = String.fromCharCode(key);
 
-      if (document.activeElement.tagName !== "BODY")
-        console.log(
-          `${event.type}:${document.activeElement.tagName}:${event.shiftKey} ${chr}`
-        );
-    });
+      if (document.activeElement.tagName !== "BODY") {
+        if (macro.length > 0) {
+          if (chr !== ":") setMacro(macro + chr);
+          else {
+            let curValue = document.activeElement.value;
+            document.activeElement.value = curValue.replace(`${macro}`, "🙏");
+            setMacro("");
+          }
+        } else if (chr === ":") {
+          setMacro(chr);
+        }
+      }
+    },
+    [macro]
+  );
+  useEffect(() => {
+    window.addEventListener("keypress", handleUserMacro);
+    window.addEventListener("keydown", handleSpecialKey);
     return () => {
-      window.removeEventListener("keypress");
+      window.removeEventListener("keypress", handleUserMacro);
+      window.removeEventListener("keydown", handleSpecialKey);
     };
-  }, []);
+  }, [handleSpecialKey, handleUserMacro]);
   return (
     <div className="App">
-      <input type="text" id="example" name="example" ref={inputEl} />
+      <textarea ref={textEl} />
+      <p>{macro}</p>
     </div>
   );
 }
