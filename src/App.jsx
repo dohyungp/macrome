4e460a31827ae0971e30761505bb24693aef1a3c
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2020-04-26 12:27:43 +0900

    Add emoji convertor

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,26 @@
-import React, { useState, useRef, useEffect, useCallback } from "react";
-import "./App.css";
+import React, { useEffect } from "react";
+import EmojiConvertor from "emoji-js";
 
 function App() {
-  const textEl = useRef(null);
-  const [macro, setMacro] = useState("");
-  const handleSpecialKey = useCallback(
-    (event) => {
-      let key = event.keyCode;
-      if (key === 8 && macro.length > 0)
-        setMacro(macro.slice(0, macro.length - 1));
-    },
-    [macro]
-  );
-  const handleUserMacro = useCallback(
-    (event) => {
-      let key = event.keyCode;
-      let chr = String.fromCharCode(key);
+  const emojifier = new EmojiConvertor();
 
-      if (document.activeElement.tagName !== "BODY") {
-        if (macro.length > 0) {
-          if (chr !== ":") setMacro(macro + chr);
-          else {
-            let curValue = document.activeElement.value;
-            document.activeElement.value = curValue.replace(`${macro}`, "🙏");
-            setMacro("");
-          }
-        } else if (chr === ":") {
-          setMacro(chr);
-        }
-      }
-    },
-    [macro]
-  );
   useEffect(() => {
-    window.addEventListener("keypress", handleUserMacro);
-    window.addEventListener("keydown", handleSpecialKey);
-    return () => {
-      window.removeEventListener("keypress", handleUserMacro);
-      window.removeEventListener("keydown", handleSpecialKey);
+    const handleUserMacro = (event) => {
+      if (document.activeElement?.tagName !== "BODY") {
+        const key = event.keyCode;
+        const curValue = document.activeElement?.value;
+        if (curValue && key === 186)
+          document.activeElement.value = emojifier.replace_colons(curValue);
+      }
     };
-  }, [handleSpecialKey, handleUserMacro]);
+    window.addEventListener("keyup", handleUserMacro);
+    return () => {
+      window.removeEventListener("keyup", handleUserMacro);
+    };
+  }, [emojifier]);
   return (
     <div className="App">
-      <textarea ref={textEl} />
-      <p>{macro}</p>
+      <textarea />
     </div>
   );
 }
